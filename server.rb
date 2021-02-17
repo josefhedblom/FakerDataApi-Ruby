4f595fc75dbe01b996b9e0e71f98aa60ca2ac127
Josef Hedblom <hedblomjosef@gmail.com>
2021-02-17 23:30:26 +0100

    Add routes to server.rb

--- server.rb
+++ server.rb
@@ -3,6 +3,7 @@ require "sinatra/namespace"
 require "sinatra/reloader"
 require 'faker'
 require 'json'
+require_relative  './lib/model/FakerData.rb'
 
 set :bind, 'localhost'
 set :port, 5000
@@ -11,4 +12,16 @@ namespace '/api/v1' do
     before do
         content_type 'application/json'
     end
+
+    get '/names/' do
+        file = File.open('./lib/FakerDataFiles/faker-data.js','r')
+        file.read
+      end
+
+      get '/names/new' do
+        data = FakerData.new
+        data.generate_data
+        data.read_file
+        redirect to('/api/v1/names/')
+      end
 end
